48409401b61fdf6d8752841261ab75b3ec1b3c4d
T-Damer <dpancov64@gmail.com>
2022-01-06 11:43:00 +0300

    fixed relative import

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'module-alias/register'
 import 'source-map-support/register'
 
 import { startBot } from '@/helpers/bot'
-import deployCommands from './helpers/deployCommands'
+import deployCommands from '@/helpers/deployCommands'
 
 void (async () => {
   await deployCommands()
